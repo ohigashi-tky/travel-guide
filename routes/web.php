3b19cdbbf01db9eb17a0adde207dd9b226e0f6c9
ohigashi-tky <97085725+ohigashi-tky@users.noreply.github.com>
2025-03-27 21:38:14 +0900

    Fix Test Code

--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,21 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\WorkOS\Http\Middleware\ValidateSessionWithWorkOS;
+use Illuminate\Support\Facades\App;
 
 Route::get('/', function () {
     return redirect('/dashboard');
 })->middleware(['auth']);
 
-Route::middleware([
-    'auth',
-    ValidateSessionWithWorkOS::class,
-])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
-});
+if (App::environment('testing')) {
+    Route::middleware(['auth'])->group(function () {
+        Route::get('dashboard', fn () => Inertia::render('Dashboard'))->name('dashboard');
+    });
+} else {
+    Route::middleware(['auth', ValidateSessionWithWorkOS::class])->group(function () {
+        Route::get('dashboard', fn () => Inertia::render('Dashboard'))->name('dashboard');
+    });
+}
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
